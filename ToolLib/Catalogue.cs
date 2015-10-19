eb90a485169d0650b9c079b9acaceff1dbff2c60
Le Nhat Anh W7 VM <lenhatanh86@gmail.com>
2015-10-19 15:29:07 +0700

    More generated sql stored procedures

--- ToolLib/Catalogue.cs
+++ ToolLib/Catalogue.cs
@@ -20,7 +20,7 @@ namespace ToolLib
             }
 
             // name and column
-            string name = dt_pre + "_" + token[0].Trim();
+            string name = dt_pre + token[0].Trim();
             List<DbColumn> columns = Catalogue.getColumns(token);
 
             // result
@@ -29,6 +29,11 @@ namespace ToolLib
             result += name;
             result += "\r\n" + Catalogue.sqlCreateDb(name, columns);
             result += "\r\n" + "\r\n" + "\r\n" + Catalogue.sqlProcSelect(name);
+            result += "\r\n" + "\r\n" + Catalogue.sqlProcInsert(name, columns);
+            result += "\r\n" + "\r\n" + Catalogue.sqlProcDel(name, columns);
+            result += "\r\n" + "\r\n" + Catalogue.sqlProcUpdate(name, columns);
+            result += "\r\n" + "\r\n" + Catalogue.sqlProcInUse(name, columns);
+            result += "\r\n" + "\r\n" + Catalogue.sqlProcExists(name, columns);
 
             return result;
         }
@@ -73,6 +78,140 @@ namespace ToolLib
             sql += "\r\n" + "select * from " + tableName;
             sql += "\r\n" + "set nocount off;";
             sql += "\r\n" + "end";
+
+            return sql;
+        }
+
+        private static string sqlProcDel(string tableName, List<DbColumn> columns)
+        {
+            string sql = "";
+
+            foreach (DbColumn col in columns)
+            {
+                if (col.PrimaryKey)
+                {
+                    sql += "create procedure " + sp_pre + tableName + "_Delete";
+                    sql += "\r\n" + "@" + col.Name + " " + col.Type;
+                    sql += "\r\n" + "as";
+                    sql += "\r\n" + "begin";
+                    sql += "\r\n" + "delete from " + tableName;
+                    sql += "\r\n" + "where " + col.Name + " = @" + col.Name;
+                    sql += "\r\n" + "end";
+                    sql += "\r\n";
+                }
+            }
+
+            return sql;
+        }
+
+        private static string sqlProcExists(string tableName, List<DbColumn> columns)
+        {
+            string sql = "";
+
+            foreach (DbColumn col in columns)
+            {
+                if (col.PrimaryKey)
+                {
+                    sql += "create procedure " + sp_pre + tableName + "_Exists";
+                    sql += "\r\n" + "@" + col.Name + " " + col.Type;
+                    sql += "\r\n" + "as";
+                    sql += "\r\n" + "begin";
+                    sql += "\r\n" + "if exists(select * from " + tableName + " where " + col.Name + " = @" + col.Name + ")";
+                    sql += "\r\n" + "select 1 as IsExists";
+                    sql += "\r\n" + "else select 0 as IsExists";
+                    sql += "\r\n" + "end";
+                    sql += "\r\n";
+                }
+            }
+
+            return sql;
+        }
+
+        private static string sqlProcInsert(string tableName, List<DbColumn> columns)
+        {
+            string sql = "";
+            List<String> declares = new List<string>();
+            List<String> cols = new List<string>();
+            List<String> vals = new List<string>();
+
+            sql += "create procedure " + sp_pre + tableName + "_Insert";
+
+            foreach (DbColumn col in columns)
+            {
+                if (!col.Id)
+                {
+                    declares.Add("@" + col.Name + " " + col.Type);
+                    cols.Add(col.Name);
+                    vals.Add("@" + col.Name);
+                }
+            }
+
+            sql += "\r\n" + String.Join(",\r\n", declares);
+
+            sql += "\r\n" + "as";
+            sql += "\r\n" + "begin";
+            sql += "\r\n insert into " + tableName + "(" + String.Join(", ", cols) + ")";
+            sql += "\r\n values (" + String.Join(", ", vals) + ")";
+            sql += "\r\n" + "end";
+            sql += "\r\n";
+
+            return sql;
+        }
+
+        private static string sqlProcInUse(string tableName, List<DbColumn> columns)
+        {
+            string sql = "";
+
+            foreach (DbColumn col in columns)
+            {
+                if (col.PrimaryKey)
+                {
+                    sql += "create procedure " + sp_pre + tableName + "_InUse";
+                    sql += "\r\n" + "@" + col.Name + " " + col.Type;
+                    sql += "\r\n" + "as";
+                    sql += "\r\n" + "begin";
+                    sql += "\r\n" + "if exists(select HoSoID from HoSo_xxxxx where " + col.Name + " = @" + col.Name + ")";
+                    sql += "\r\n" + "select 1 as InUse";
+                    sql += "\r\n" + "else select 0 as InUse";
+                    sql += "\r\n" + "end";
+                    sql += "\r\n";
+                }
+            }
+
+            return sql;
+        }
+
+        private static string sqlProcUpdate(string tableName, List<DbColumn> columns)
+        {
+            string sql = "";
+            List<String> declares = new List<string>();
+            List<String> updates = new List<string>();
+            string where = "";
+
+            sql += "create procedure " + sp_pre + tableName + "_Update";
+
+            foreach (DbColumn col in columns)
+            {
+                declares.Add("@" + col.Name + " " + col.Type);
+
+                if (!col.PrimaryKey)
+                {
+                    updates.Add(col.Name + " = @" + col.Name);
+                } else
+                {
+                    where = col.Name + " = @" + col.Name;
+                }
+            }
+
+            sql += "\r\n" + String.Join(",\r\n", declares);
+
+            sql += "\r\n" + "as";
+            sql += "\r\n" + "begin";
+            sql += "\r\n update " + tableName;
+            sql += "\r\n set " + String.Join(", ", updates);
+            sql += "\r\n where " + where;
+            sql += "\r\n" + "end";
+            sql += "\r\n";
 
             return sql;
         }
