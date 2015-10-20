e0c8894528bc778dc5a678d81feacf455d44be08
Le Nhat Anh W7 VM <lenhatanh86@gmail.com>
2015-10-20 23:55:45 +0700

    Add generated code for DataLayer

--- ToolLib/Database.cs
+++ ToolLib/Database.cs
@@ -38,6 +38,18 @@ namespace ToolLib
             result += "\r\n" + "\r\n" + Database.codeMetaData(name, columns);
             result += "\r\n" + "\r\n" + Database.codeCommandList(name);
 
+            string codeInsert = Database.codeDataLayerInsert(name, columns);
+            string codeInUse = Database.codeDataLayerInUse(name, columns);
+
+            result += "\r\n" + Database.codeDataLayerOpen(name);
+            result += "\r\n" + Database.codeDataLayerSelect(name);
+            result += "\r\n" + codeInsert;
+            result += "\r\n" + Database.codeDataLayerUpdate(codeInsert);
+            result += "\r\n" + Database.codeDataLayerDelete(codeInUse);
+            result += "\r\n" + codeInUse;
+            result += "\r\n" + Database.codeDataLayerExists(codeInUse);
+            result += "\r\n}";
+
             return result;
         }
 
@@ -200,7 +212,8 @@ namespace ToolLib
                 if (!col.PrimaryKey)
                 {
                     updates.Add(col.Name + " = @" + col.Name);
-                } else
+                }
+                else
                 {
                     where = col.Name + " = @" + col.Name;
                 }
@@ -252,10 +265,160 @@ namespace ToolLib
             code += "\r\npublic const string " + tableName.Replace("DT_", "") + "_Delete = \"" + Database.sp_pre + tableName + "_Delete\";";
             code += "\r\npublic const string " + tableName.Replace("DT_", "") + "_InUse = \"" + Database.sp_pre + tableName + "_InUse\";";
             code += "\r\npublic const string " + tableName.Replace("DT_", "") + "_Exists = \"" + Database.sp_pre + tableName + "_Exists\";";
-            
+
             code += "\r\n#endregion";
 
             return code;
+        }
+
+        private static string codeDataLayerSelect(string tableName)
+        {
+            string code = @"
+                public DataTable Select()
+                {{
+                    try
+                    {{
+                        DbCommand cmd = CreateCommand(CommandList.{0}, CommandType.StoredProcedure);
+                        DataTable dt = FillDataTable(cmd, MetaData.{1});
+                        return dt;
+                    }}
+                    catch (Exception ex)
+                    {{
+                        throw ex;
+                    }}
+                }}";
+
+            string command = tableName + "_Select";
+            string meta = getMetaDataName(tableName);
+
+            return String.Format(code, command, meta);
+        }
+
+        private static string codeDataLayerInsert(string tableName, List<DbColumn> columns)
+        {
+            string code = @"
+                public bool Insert({0})
+                {{
+                    try
+                    {{
+                        using (DbCommand cmd = CreateCommand(CommandList.{1}, CommandType.StoredProcedure))
+                        {{
+                            {2}
+                            int rowAffected = ExecuteNonQuery(cmd);
+                            return rowAffected > 0;
+                        }}
+                    }}
+                    catch (Exception ex)
+                    {{
+                        return false;
+                    }}
+                }}";
+
+            List<String> declaredCmdParams = new List<string>();
+            List<String> declaredParams = new List<string>();
+            string command = tableName + "_Insert";
+
+            foreach (DbColumn col in columns)
+            {
+                declaredParams.Add("string " + col.Name);
+                declaredCmdParams.Add("cmd.Parameters.Add(CreateParameter(\"@\" + MetaData." + getMetaDataName(tableName, col.Name) + ", DbType.String, " + col.Name + "));");
+            }
+
+            return String.Format(code, String.Join(", ", declaredParams), command, String.Join("\r\n", declaredCmdParams));
+        }
+
+        private static string codeDataLayerUpdate(string insertCode)
+        {
+            return insertCode.Replace("Insert", "Update");
+        }
+
+        private static string codeDataLayerUpdate(string tableName, List<DbColumn> columns)
+        {
+            return codeDataLayerUpdate(codeDataLayerInsert(tableName, columns));
+        }
+
+        private static string codeDataLayerInUse(string tableName, List<DbColumn> columns)
+        {
+            string code = @"
+                public bool InUse({0})
+                {{
+                    try
+                    {{
+                        DbCommand cmd = CreateCommand(CommandList.{1}, CommandType.StoredProcedure);
+                        {2}
+                        return Helper.ConvertToBool(ExecuteScalar(cmd));
+                    }}
+                    catch (Exception ex)
+                    {{
+                        return true;
+                    }}
+                }}";
+
+            List<String> declaredCmdParams = new List<string>();
+            List<String> declaredParams = new List<string>();
+            string command = tableName + "_InUse";
+
+            foreach (DbColumn col in columns)
+            {
+                if (col.PrimaryKey)
+                {
+                    declaredParams.Add("string " + col.Name);
+                    declaredCmdParams.Add("cmd.Parameters.Add(CreateParameter(\"@\" + MetaData." + getMetaDataName(tableName, col.Name) + ", DbType.String, " + col.Name + "));");
+                }
+            }
+
+            return String.Format(code, String.Join(", ", declaredParams), command, String.Join("\r\n", declaredCmdParams));
+        }
+
+        private static string codeDataLayerExists(string inUseCode)
+        {
+            return inUseCode.Replace("InUse", "Exists");
+        }
+
+        private static string codeDataLayerExists(string tableName, List<DbColumn> columns)
+        {
+            return codeDataLayerExists(codeDataLayerInUse(tableName, columns));
+        }
+
+        private static string codeDataLayerDelete(string inUseCode)
+        {
+            return inUseCode.Replace("InUse", "Delete").Replace("return true;", "return false;").Replace("return Helper.ConvertToBool(ExecuteScalar(cmd));", "ExecuteNonQuery(cmd);\r\nreturn true;");
+        }
+
+        private static string codeDataLayerDelete(string tableName, List<DbColumn> columns)
+        {
+            return codeDataLayerDelete(codeDataLayerInUse(tableName, columns));
+        }
+
+        private static string codeDataLayerOpen(string tableName)
+        {
+            string code = @"
+                public class {0} : BaseData
+                {{
+                    public {0}(string provider, string connectionString)
+                        : base(provider, connectionString)
+                    {{}}
+
+                    public {0}(string connectionString)
+                        : base(connectionString)
+                    {{}}";
+
+            return String.Format(code, tableName);
+        }
+
+        private static string getMetaDataName(string name)
+        {
+            return name.Replace("DT_", "").ToUpper();
+        }
+
+        private static string getMetaDataName(string name1, string name2)
+        {
+            return name1.Replace("DT_", "").ToUpper() + "_" + name2.ToUpper();
+        }
+
+        private static string getCommandListName(string name)
+        {
+            return name.Replace("DT_", "");
         }
 
         private static List<DbColumn> getColumns(string[] token)
